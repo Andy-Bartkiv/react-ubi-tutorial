e09d9af825d068b55d876ec751455664ac670d84
Andy Bartkiv <andy.bartkiv@gmail.com>
2021-12-20 21:55:17 +0300

    add comments fetching for eqch post

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default class PostService {
+
     static async getAll(limit = 10, page = 1) {
         const resposne = await axios.get('https://jsonplaceholder.typicode.com/posts', {
             params: {
@@ -10,4 +11,16 @@ export default class PostService {
         });
         return resposne;
     }
+
+    static async getPostByID(id) {
+        const resposne = await axios.get('https://jsonplaceholder.typicode.com/posts/' + id);
+        return resposne;
+    }
+
+    static async getCommentsByID(id) {
+        const resposne = await axios.get('https://jsonplaceholder.typicode.com/posts/' + id + '/comments');
+        return resposne;
+    }
+
 }
+
--- src/pages/PostIDPage.js
+++ src/pages/PostIDPage.js
@@ -1,12 +1,56 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import PostService from '../API/PostService';
+import Loader from '../components/UI/loader/Loader';
+import { useFetch } from '../hooks/useFetch';
 
 const PostIDPage = () => {
-
+    
     const postID = useParams().id;
+    const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+    const [fetchPostByID, isLoading, error] = useFetch( async (id) => {
+        const resp = await PostService.getPostByID(id);
+        setPost(resp.data)
+    });
+    const [fetchCommentsByID, isComentsLoading, errorComments] = useFetch( async (id) => {
+        const resp = await PostService.getCommentsByID(id);
+        await console.log(resp);
+        setComments(resp.data);
+    });
+
+    useEffect( () => {
+        fetchPostByID(postID);
+        fetchCommentsByID(postID);
+    }, []);
 
     return (
         <div className='App-body'>
-            You open POST ID = { postID }
+            
+            <br/>
+            { (isLoading)
+                ? <Loader />
+                : <div>
+                    <h3> { post.id}. { post.title }</h3>
+                    <hr style={{ border: 'none', margin: '.25em 0', height: '.05em', background: 'whitesmoke' }}/>
+                    <p>{ post.body }</p>
+                  </div>
+            }
+            <br/>
+            <h4>Comments:</h4>
+        
+            { (isComentsLoading)
+                ? <Loader />
+                : <ol style={{ padding: '.25em 1em'}}>
+                    { comments.map(cmnt =>
+                        <li>
+                            <h5>{cmnt.email}:</h5>
+                            <p>{cmnt.name}</p>
+                        </li>
+                    )}
+                  </ol>
+            }
+                
         </div>
     )
 }
